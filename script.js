5374177cbda5e2011cc92456cadcda5fddf85329
Di-Werman <118116167+Di-Werman@users.noreply.github.com>
2023-02-20 17:07:33 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// class -> .CLASS_NAME
+//id ->#ID
+// tag -> TAG_NAME
+
+var a = document.querySelector('a')
+var oldHref = a.getAttribute('href')
+
+a.setAttribute('href','https://ya.ru')
+a.setAttribute('title','Go to Yandex')
+a.textContent = 'Yandex'
+
+console.log(a.attributes)
